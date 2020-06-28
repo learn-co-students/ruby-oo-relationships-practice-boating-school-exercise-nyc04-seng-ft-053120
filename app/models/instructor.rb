8621d7a5ee4b88d5b9a3934c27189aa95c9f19f3
mariamoljames <mariamol.james@gmail.com>
2020-06-28 15:23:22 -0400

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,35 @@
 class Instructor
+    attr_accessor :name
+    @@all=[]
+    def initialize(name)
+        @name=name
+        @@all<<self
+    end
+    
+    def pass_student(student,test_name)
+        BoatingTest.all.find do |bt| 
+            if bt.student==student and bt.test_name==test_name
+                bt.test_status="passed"
+                bt
+            else
+                bt=BoatingTest.new(student,test_name,"passed",self)
+            end
+        end
+    end
+
+    def fail_student(student,test_name)
+        BoatingTest.all.find do |bt| 
+            if bt.student==student and bt.test_name==test_name
+                bt.test_status="failed"
+                bt
+            else
+                bt=BoatingTest.new(student,test_name,"failed",self)
+            end
+        end
+    end
+
+    def self.all
+        @@all
+    end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,26 @@
 class Student
+    attr_accessor :first_name
+    @@all=[]
+    def initialize(first_name)
+        @first_name=first_name
+        @@all<<self
+    end
+    def add_boating_test(test_name,test_status,instructor)
+     boating_test=BoatingTest.new(self,test_name,test_status,instructor)
+    end
+    
+    def grade_percentage
+        total=BoatingTest.all.select {|bt| bt.student.first_name==self.first_name}
+        passed=BoatingTest.all.select {|bt| bt.student.first_name==self.first_name and bt.test_status=="passed"}
+        percentage=((passed.length.to_f)/(total.length.to_f))*100
+    end
+    
+    def self.find_student(first_name)
+        Student.all.find {|student| student.first_name==first_name}
+    end
+    def self.all
+        @@all
+    end
+
 
 end
