d1f4285aba3b52a6f04c204a503c22d716efd305
peawarrior <jacksonchen171@gmail.com>
2020-06-28 12:48:14 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,45 @@
 class Student
+    attr_reader :first_name
+    @@all = []
 
+    def initialize(first_name)
+        @first_name = first_name
+        save
+    end
+
+    def save
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(name, status, instructor)
+        BoatingTest.new(self, name, status, instructor)
+    end
+
+    def self.find_student(first_name)
+        self.all.find do |student|
+            student.first_name == first_name
+        end
+    end
+
+    def boatingtests
+        BoatingTest.all.select do |boatingtest|
+            boatingtest.student == self
+        end
+    end
+
+    def total_passed
+        passed = 0
+        boatingtests.each do |boatingtest|
+            passed += 1 if boatingtest.status == "passed"
+        end
+        passed.to_f
+    end
+
+    def grade_percentage
+        (total_passed/boatingtests.count)*100
+    end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -8,6 +8,17 @@ end
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
 
+s1 = Student.new("Billy")
+s2 = Student.new("John")
+s3 = Student.new("Samantha")
+s4 = Student.new("Jasmine")
+
+i1 = Instructor.new("Instructor Jeff")
+i2 = Instructor.new("Instructor Cindy")
+
+s1.add_boating_test("Initial", "pending", i1)
+s2.add_boating_test("Initial", "pending", i1)
+
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
 
